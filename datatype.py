9937bf3eb335564b5310465c7e99f479c7e2e09d
Rishu Raj Singh <rishurajsingh170303@gmail.com>
2026-05-19 16:28:31 +0530

    Update datatype file

--- datatype.py
+++ datatype.py
@@ -3,9 +3,24 @@
 #print(type(num))
 #print(5**2)
 #increment value
-num = 1
+#num = 1
 #num = num +1
 #num += 1
 #print(num)
 #print(abs(-3))
-#print(round(3.14555, 1))
+#print(round(3.9))
+#print(round(9.677,1))
+
+#num1 =5
+#um2 = 3
+"""print(num1/num2)
+print(num1==num2)
+print(num1>num2)
+print(num1!=num2)
+print(num1>=num2)
+print(num1 <= num2)"""
+
+num1="555"
+num2="125"
+#print(num1+num2)
+print(int(num1)+int(num2))
--- string.py
+++ string.py
@@ -1,7 +1,18 @@
-mess = "Hello World "
-mess.replace("World","INDIA")
+mess = "Hello World"
+#mess_len=len(mess)
+print(mess[-1])
+#mess.replace("World","INDIA")
+#new_mess = mess.replace("World","INDIA")
+#print(mess)
+#print(new_mess)
 
-print(mess)
+
+#gretting = "Hello"
+#name = "Rishu"
+#message = f"{gretting} {name.upper()} . Welcome to Python"
+#print(message)
+#print (dir(name))
+#print(help(name.upper))
 
 
 
